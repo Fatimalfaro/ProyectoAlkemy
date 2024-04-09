5bb203d10c9e90f161d0c50616545b8d308eda37
Fátima Alfaro <fatialfaro99@gmail.com>
2024-04-09 15:48:23 -0300

    Se configuro admin

--- compra/admin.py
+++ compra/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Proveedor, Producto
 
 # Register your models here.
+class ProveedorAdmin(admin.ModelAdmin):
+    list_display=["nombre", "apellido", "dni"]
+    search_fields=["nombre", "apellido", "dni"]
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display=["nombre", "precio", "stock_actual"]
+    search_fields=["nombre", "precio", "stock_actual"]
+
+admin.site.register(Proveedor,ProveedorAdmin),
+admin.site.register(Producto,ProductoAdmin)
+
+#USER: FATIMAALFARO
+#PASSWORD:2586
