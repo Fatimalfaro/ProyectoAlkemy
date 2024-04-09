54fdc5756e05b94e437e383e589570968647ed67
Fátima Alfaro <fatialfaro99@gmail.com>
2024-04-09 14:56:18 -0300

    Se agrega listar producto con su respectiva tabla y se agrega el bonus de listar proveedores

--- compra/urls.py
+++ compra/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('crearProveedor/', views.nuevo_proveedor),
     path('crearProducto/', views.nuevo_producto), 
+    path('listaProductos', views.mostrar_productos),
+    path('listaProveedores', views.mostrar_proveedores),
+
 ]
--- compra/views.py
+++ compra/views.py
@@ -5,6 +5,9 @@ from .formsproducto import ProductoForm
 from .models import Producto, Proveedor
 from django.template import Template, Context
 
+
+#Realizo el CRUD de proveedor y producto 
+#Crear
 def nuevo_proveedor(request):
     if request.method == 'POST':
         form = ProveedorForm(request.POST)
@@ -32,3 +35,30 @@ def nuevo_producto(request):
             proveedor_id=proveedor_id,
         )
     return render(request, 'crear_producto.html', context)
+
+#Leer/Mostrar
+#Mostrar todos los productos
+def mostrar_productos(request):
+    productos = Producto.objects.all()
+    #return render(request,'lista_productos.html', {'productos': productos})
+    ruta = "C:/Users/fatia/OneDrive/Escritorio/SDGP3/stock/com/templates/listado_productos.html"
+    doc_externo = open(ruta)
+    plantilla = Template(doc_externo.read())
+    doc_externo.close()
+    contexto = Context({'productos': productos})
+    documento = plantilla.render(contexto)
+    return HttpResponse(documento)
+
+#Mostrar todos los proveedores
+def mostrar_proveedores(request):
+    proveedores = Proveedor.objects.all()
+    #return render(request,'lista_productos.html', {'productos': productos})
+    ruta = "C:/Users/fatia/OneDrive/Escritorio/SDGP3/stock/com/templates/listado_proveedores.html"
+    doc_externo = open(ruta)
+    plantilla = Template(doc_externo.read())
+    doc_externo.close()
+    contexto = Context({'proveedores': proveedores})
+    documento = plantilla.render(contexto)
+    return HttpResponse(documento)
+
+
