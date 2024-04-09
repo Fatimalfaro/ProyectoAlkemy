f0d789540a1fa283e589e43b0b853d512499665b
Fátima Alfaro <fatialfaro99@gmail.com>
2024-04-09 15:35:52 -0300

    Se completo el CRUD, se puede ademas filtrar, actualizar y borrar productos y proveedores

--- compra/urls.py
+++ compra/urls.py
@@ -6,5 +6,12 @@ urlpatterns = [
     path('crearProducto/', views.nuevo_producto), 
     path('listaProductos', views.mostrar_productos),
     path('listaProveedores', views.mostrar_proveedores),
-
+    path('actualizarProducto/<str:nombre>', views.update_producto_nombre),
+    path('actualizarProveedor/<int:dni>', views.update_proveedor_dni),
+    path('filtrarProducto/<int:precio>', views.filtrar_productos_precio),
+    path('filtrarProveedor/<int:dni>', views.filtrar_proveedor_dni),
+    path('borrarProducto/<str:nombre>', views.delete_producto_nombre), 
+    path('borrarProductos', views.delete_all),
+    path('borrarProveedor/<int:dni>', views.delete_proveedor_dni), 
+    path('borrarProveedores', views.delete_all),
 ]
--- compra/views.py
+++ compra/views.py
@@ -40,8 +40,7 @@ def nuevo_producto(request):
 #Mostrar todos los productos
 def mostrar_productos(request):
     productos = Producto.objects.all()
-    #return render(request,'lista_productos.html', {'productos': productos})
-    ruta = "C:/Users/fatia/OneDrive/Escritorio/SDGP3/stock/com/templates/listado_productos.html"
+    ruta = "C:/Users/fatia/OneDrive/Escritorio/Proyecto - Alkemy/StockControl/compra/templates/listar_productos.html"
     doc_externo = open(ruta)
     plantilla = Template(doc_externo.read())
     doc_externo.close()
@@ -49,11 +48,10 @@ def mostrar_productos(request):
     documento = plantilla.render(contexto)
     return HttpResponse(documento)
 
-#Mostrar todos los proveedores
+#Mostrar todos los proveedores/Bonus
 def mostrar_proveedores(request):
     proveedores = Proveedor.objects.all()
-    #return render(request,'lista_productos.html', {'productos': productos})
-    ruta = "C:/Users/fatia/OneDrive/Escritorio/SDGP3/stock/com/templates/listado_proveedores.html"
+    ruta = "C:/Users/fatia/OneDrive/Escritorio/Proyecto - Alkemy/StockControl/compra/templates/listar_proveedores.html"
     doc_externo = open(ruta)
     plantilla = Template(doc_externo.read())
     doc_externo.close()
@@ -61,4 +59,108 @@ def mostrar_proveedores(request):
     documento = plantilla.render(contexto)
     return HttpResponse(documento)
 
+#Actualizar
+#Actualizar proveedor por su dni
+def update_proveedor_dni(request, dni):
+    proveedorActualizar = Proveedor.objects.get(dni = dni)
+    proveedorActualizar.nombre = "Pamela"
+    proveedorActualizar.apellido = "Sanchez"
+    proveedorActualizar.dni = 1254789
+    proveedorActualizar.save()
+    ruta = "C:/Users/fatia/OneDrive/Escritorio/Proyecto - Alkemy/StockControl/compra/templates/proveedores_actualizados.html"
+    doc_externo = open(ruta)
+    plantilla = Template(doc_externo.read())
+    doc_externo.close()
+    contexto = Context({'proveedorActualizar': proveedorActualizar})
+    documento = plantilla.render(contexto)
+    return HttpResponse(documento)
+
+#Actualizar producto por su nombre
+def update_producto_nombre(request, nombre):
+    productoActualizar = Producto.objects.get(nombre = nombre)
+    productoActualizar.nombre = "Heladera"
+    productoActualizar.precio = 800000
+    productoActualizar.stock_actual = 5
+    productoActualizar.save()
+    ruta = "C:/Users/fatia/OneDrive/Escritorio/Proyecto - Alkemy/StockControl/compra/templates/productos_actualizados.html"
+    doc_externo = open(ruta)
+    plantilla = Template(doc_externo.read())
+    doc_externo.close()
+    contexto = Context({'productoActualizar':  productoActualizar})
+    documento = plantilla.render(contexto)
+    return HttpResponse(documento)
+
+#Filtrar
+#Mostrar productos filtrados por precio
+def filtrar_productos_precio(request, precio):
+    productoEncontrado = Producto.objects.filter(precio=precio)
+    ruta = "C:/Users/fatia/OneDrive/Escritorio/Proyecto - Alkemy/StockControl/compra/templates/productos_filtrados.html"
+    doc_externo = open(ruta)
+    plantilla = Template(doc_externo.read())
+    doc_externo.close()
+    contexto = Context({'productoEncontrado': productoEncontrado})
+    documento = plantilla.render(contexto)
+    return HttpResponse(documento)
+
+#Mostrar proveedores filtrados por dni - R
+def filtrar_proveedor_dni(request, dni):
+    proveedorEncontrado = Proveedor.objects.filter(dni=dni)
+    ruta = "C:/Users/fatia/OneDrive/Escritorio/Proyecto - Alkemy/StockControl/compra/templates/proveedores_filtrados.html"
+    doc_externo = open(ruta)
+    plantilla = Template(doc_externo.read())
+    doc_externo.close()
+    contexto = Context({'proveedorEncontrado': proveedorEncontrado})
+    documento = plantilla.render(contexto)
+    return HttpResponse(documento)
+
+#Borrar
+#Borrar todos los proveedores 
+def delete_all(request):
+    proveedores = Proveedor.objects.all()
+    proveedores.delete()
+    ruta = "C:/Users/fatia/OneDrive/Escritorio/Proyecto - Alkemy/StockControl/compra/templates/listar_proveedores.html"
+    doc_externo = open(ruta)
+    plantilla = Template(doc_externo.read())
+    doc_externo.close()
+    contexto = Context({'proveedores':  proveedores})
+    documento = plantilla.render(contexto)
+    return HttpResponse(documento)
+
+#Borrar proveedores por su dni 
+def delete_proveedor_dni(request, dni):
+    proveedorBorrar = Proveedor.objects.get(dni = dni)
+    proveedorBorrar.delete()
+    proveedores = Proveedor.objects.all()
+    ruta = "C:/Users/fatia/OneDrive/Escritorio/Proyecto - Alkemy/StockControl/compra/templates/listar_proveedores.html"
+    doc_externo = open(ruta)
+    plantilla = Template(doc_externo.read())
+    doc_externo.close()
+    contexto = Context({'proveedores':  proveedores})
+    documento = plantilla.render(contexto)
+    return HttpResponse(documento)
+
+#Borrar todos los productos 
+def delete_all(request):
+    productos = Producto.objects.all()
+    productos.delete()
+    ruta = "C:/Users/fatia/OneDrive/Escritorio/Proyecto - Alkemy/StockControl/compra/templates/listar_productos.html"
+    doc_externo = open(ruta)
+    plantilla = Template(doc_externo.read())
+    doc_externo.close()
+    contexto = Context({'productos' : productos})
+    documento = plantilla.render(contexto)
+    return HttpResponse(documento)
+
+#Borrar producto por su nombre
+def delete_producto_nombre(request, nombre):
+    productoBorrar = Producto.objects.get(nombre = nombre)
+    productoBorrar.delete()
+    productos = Producto.objects.all()
+    ruta = "C:/Users/fatia/OneDrive/Escritorio/Proyecto - Alkemy/StockControl/compra/templates/listar_productos.html"
+    doc_externo = open(ruta)
+    plantilla = Template(doc_externo.read())
+    doc_externo.close()
+    contexto = Context({'productos':  productos})
+    documento = plantilla.render(contexto)
+    return HttpResponse(documento)
 
